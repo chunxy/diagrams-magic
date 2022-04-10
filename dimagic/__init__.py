b96fcde3068d873204bf00ea5a225dd67b2156a8
unknown <yechunxiao269@gmail.com>
2022-04-10 18:30:24 +0800

    Remove extra-generated files

--- dimagic/__init__.py
+++ dimagic/__init__.py
@@ -1,11 +1,10 @@
-
-from IPython.core.magic import register_cell_magic
 from IPython.core.interactiveshell import InteractiveShell
 from IPython.display import display, SVG
-from os import  popen, name
-from subprocess import (DEVNULL, check_output, call, SubprocessError)
+from os import remove
+from subprocess import check_output, call, SubprocessError
 import platform
 from shutil import which
+from datetime import date
 
 def diagrams(line: str, cell: str):
     if which('diagrams') is None:
@@ -16,16 +15,16 @@ def diagrams(line: str, cell: str):
     if line not in drivers:
         return 'use as: %%diagrams <driver name>, supported drivers are {:s}'.format(str(drivers))
 
-    src = 'graph.txt'
-    dest = 'graph.svg'
+    now = str(date.today())
+    src = now + '-src.txt'
+    dest = now + '-dest.svg'
 
     with open(src, 'w') as desc:
         desc.write(cell)
-    if name == 'nt':
-        tokens = ['diagrams.cmd', line, src, dest]
-    else:
-        tokens = ['diagrams', line, src, dest]
 
+    tokens = ['diagrams', line, src, dest]
+    if platform.system().lower() == 'windows':
+        tokens[0] = 'diagrams.cmd'
     if platform.system().lower() == 'linux' and (line == 'flowchart' or line == 'sequence'):
         try:
             DISPLAY = check_output(['echo ${DISPLAY}', '-l'], shell=True).decode('utf-8')
@@ -44,7 +43,9 @@ def diagrams(line: str, cell: str):
     
     with open(dest, 'rb') as pic:
         raw = pic.read()
-
+    remove(src)
+    remove(dest)
+    
     return display(SVG(raw))
 
 def load_ipython_extension(ipython: InteractiveShell):
